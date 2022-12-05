d32da8050a0372622406dd7b58f3c14a7480a25a
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-12-05 08:28:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+    int n;
+    int i;
+    bool ris;
+    cin>>n;
+    i=n-1;
+    ris=num_prim(n,i);
+    if (ris==1)
+    {
+        cout<<"numero primo";
+    }
+    else{
+        cout<<"numero non primo";
+        }
   return 0;
 }
